1ca69a691eaa68cf275868d09c30daca7dd4756a
ValluetCP <cynthia.petitot@gmail.com>
2024-02-26 16:46:52 +0100

    Personnalisation nav - Lien actif

--- js/espace_navigation.js
+++ js/espace_navigation.js
@@ -4,7 +4,17 @@ function showList(listClassName){
         list.classList.add('hidden');
     });
 
+    // Supprimer la classe active de tous les liens de navigation
+    var navLinks = document.querySelectorAll('.nav2_menu ul li a');
+    navLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
     // Afficher la liste correspondante
     var selectedList = document.querySelector('.' + listClassName);
     selectedList.classList.remove('hidden');
+
+    // Ajouter la classe active au lien de navigation cliqué
+    var clickedLink = document.querySelector('a[href="#"][onclick="showList(\'' + listClassName + '\')"]');
+    clickedLink.classList.add('active');
 }
