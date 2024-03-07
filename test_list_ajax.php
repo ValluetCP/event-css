cb9a9afadab2e10f6636f3cadf622a159d29b913
ValluetCP <cynthia.petitot@gmail.com>
2024-03-07 11:22:13 +0100

    Page Liste des événements - Responsive
    test_list_ajax.php
    style.css

--- test_list_ajax.php
+++ test_list_ajax.php
@@ -29,13 +29,10 @@
         <div class="titreListEvent">
             <h1>tous nos événements</h1>
             <h2>sont à découvrir</h2>
-            <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis placeat inventore, voluptates reprehenderit repellat velit esse cupiditate perspiciatis minus porro tenetur. Eaque consequuntur fugit culpa facilis ut quibusdam repudiandae modi provident voluptate quasi laborum deleniti sunt, porro tenetur illum commodi. Blanditiis fuga sed, laudantium provident totam itaque eius nostrum vitae!</p> -->
 
-            <!-- <p>Assister à des représentations privé (concert, ), privatiser des lieux insolite et d'exceptions, se retrouver en petit commité pour assister à des compétitions de haut-vol, en face de vos athlètes préférés, être/ se retrouver dans la peau d'une star internationale ou encore d'un pilote de fomule 1 le temps d'une journée. Des atliers avec pour professeur un référent chacun dans son domaine culinaires qui vous  Un tête à tête avec notre sélection des plus grands chefs étoilés livreront leur plus beaux secrets pour épaté vos proches. Prendre place aux meilleurs table pour un brunch, assister à des shows culinaires.</p> -->
-
-            <p>
+            <!-- <p>
                 Plongez dans un monde d'expériences exclusives et inoubliables où chaque moment est une découverte. Assistez à des concerts privés, privatisez des lieux insolites et d'exception, et vivez des compétitions de haut vol en compagnie de vos athlètes préférés. Transformez-vous en star internationale ou en pilote de Formule 1 le temps d'une journée. Laissez-vous guider par des chefs étoilés qui dévoileront leurs secrets culinaires lors d'ateliers intimes. Savourez des brunchs aux meilleures tables et émerveillez-vous devant des shows culinaires spectaculaires. Offrez-vous des instants uniques où le luxe et l'exclusivité se marient pour créer des souvenirs inoubliables.
-            </p>
+            </p> -->
 
             <!-- catégorie : divertissement, atelier, gastronomie, représentation, loisir -->
         </div>
@@ -50,24 +47,30 @@
         <!-- CONTAINER GLOBAL - liste des events -->
         <div class="container_listEvent  menu_event">
 
-            <!-- btn Prochainement - btn historique -->
-            <a href="" id="reinitialiser_resultat">Prochainement</a>
-            <a href="" id="prochain_event">Historique</a>
-            
-            <!-- FILTRE -->
-            <div class="filtreCategory">
-                <button type="submit" class="lb_filtre">Filtrer</button>
-                <div class="lb_selectFiltre">
-                  <select name="lb_categoryFiltre" id="lb_categoryFiltre">
-                    <option value="1">Toutes les catégories</option>
-                    <option value="1">Divertissement</option>
-                    <option value="1">Atelier</option>
-                  </select>
+            <!-- ZONE FILTRE -->
+            <div class="container_btnFiltre_listEvent">
+
+                <!-- btn Prochainement - btn historique -->
+                <div class="btnProchainHistorique">
+                    <a href="" id="reinitialiser_resultat" class="prochainement_listEvent">Prochainement</a>
+                    <a href="" id="prochain_event" class="historique_listEvent">Historique</a>
+                </div>
+                
+                <!-- FILTRE -->
+                <div class="filtreCategory">
+                    <button type="submit" class="lb_filtre">Filtrer</button>
+                    <div class="lb_selectFiltre">
+                      <select name="lb_categoryFiltre" id="lb_categoryFiltre">
+                        <option value="1">Toutes les catégories</option>
+                        <option value="2">Divertissement</option>
+                        <option value="3">Atelier</option>
+                      </select>
+                    </div>
                 </div>
             </div>
 
             <!-- Div vide pour afficher le contenu -->
-            <div id="resultat">   
+            <div id="resultat" class="overflow_listEvent">   
                 <!-- MODULE 1 -->
                 <!-- MODULE pour la boucle -->
                 <div class="module_listEvent">
@@ -133,6 +136,43 @@
                         <div class="etat_listEvent">Réservé</div>
                     </div>
                 </div>
+    
+                <!-- MODULE 3 -->
+                <!-- MODULE pour la boucle -->
+                <div class="module_listEvent">
+    
+                    <!-- EMMET -->
+                    <!-- .img_listEvent>img^.txt_container_listEvent>.num_listEvent+.txt_listEvent^.tarif_etat_listEvent>.tarif_listEvent+.etat_listEvent -->
+                    
+                    <!-- MODULE - partie gauche - image -->
+                    <div class="img_listEvent"><img src="./img/event_animal.jpg" alt=""></div>
+    
+                    <!-- MODULE - partie centrale - texte -->
+                    <div class="center_txt_listEvent">
+                        <div class="txt_container_listEvent">
+                            <!-- numéro -->
+                            <div class="num_listEvent">03</div>
+                            <div class="txt_listEvent">
+                                <div class="titre_listEvent">Salon de l'automobile</div>
+                                <!-- date / category -->
+                                <div class="ss_titre_listEvent">
+                                    <div class="category">Divertissement</div>
+                                    <div class="date">29-05-2024</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <!-- MODULE - partie droite - tarif & état -->
+                    <div class="tarif_etat_listEvent">
+                        <div class="tarif_listEvent">Tarif: 75€</div>
+                        <div class="etat_listEvent">Réservé</div>
+                    </div>
+                </div>
+                <div class="degrade_listEvent">
+            </div>
+
+
             </div>
         </div>
 
@@ -181,7 +221,7 @@
                 // d) utiliser la méthode ajax de jquery pour l'affichage de la réponse
                 $.ajax({
                     url: "traitement/traitement_ajax.php", // le fichier cible, celui qui fera le traitement (projet : mettre le chemin que l'on aurait mis dans la balise <a>)
-                    type: "GET", // la méthode utilisée (projet : ne rien mettre, par défaut on sera sur la method GET)
+                    type: "POST", // la méthode utilisée (projet : ne rien mettre, par défaut on sera sur la method GET)
                     // les paramètres à fournir ex : ...id=4&nom=anonyme...(projet : on ne met rien) 
                     dataType: 'json', // le format des données attendues en tableau JSON pour être interprété et éxécuté par AJAX (projet : 'json') 
                     success: function(response) {
@@ -190,8 +230,8 @@
                         $('#resultat').html(response.contenu);
                     },
                     error: function (xhr, ajaxOptions, thrownError) {
-                        alert(xhr.status);
-                        alert(thrownError);
+                        console.log(xhr.status);
+                        console.log(thrownError);
                     }
                 });
             });
